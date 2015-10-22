a5c78853e53dcf366b53a42c8007bda2683a2d35
ethwynn <fla52310@hotmail.fr>
2015-10-22 10:46:41 +0200

    Starting LCD testing

---
+++ serial.c
@@ -0,0 +1,54 @@
+/** Functions for serial port **/
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "serial.h"
+
+static int send_serial_printf(char c,FILE *stream);
+static FILE mystdout=FDEV_SETUP_STREAM(send_serial_printf,NULL,_FDEV_SETUP_WRITE);
+
+void init_serial(int speed){
+/* Set baud rate */
+UBRR0 = F_CPU/(((unsigned long int)speed)<<4)-1;
+
+/* Enable transmitter & receiver */
+UCSR0B = (1<<TXEN0 | 1<<RXEN0);
+
+/* Set 8 bits character and 1 stop bit */
+UCSR0C = (1<<UCSZ01 | 1<<UCSZ00);
+
+/* Set off UART baud doubler */
+//UCSR0A &= ~(1 << U2X0);
+}
+
+void send_serial(char c){
+loop_until_bit_is_set(UCSR0A, UDRE0);
+UDR0 = c;
+}
+
+char get_serial(void){
+loop_until_bit_is_set(UCSR0A, RXC0);
+return UDR0;
+}
+
+void init_printf(void){
+init_serial(9600);
+stdout=&mystdout;
+}
+
+static int send_serial_printf(char c,FILE *stream){
+if(c=='\n') send_serial('\r');
+send_serial(c);
+return 0;
+}
+
+int serial_string(char * str){  
+	
+	int i;
+	for(i=0;i<strlen(str);i++){
+		send_serial(str[i]);
+	}
+return 0;
+}  
---
+++ serial.h
@@ -0,0 +1,7 @@
+/** Definitions for serial port **/
+
+void init_serial(int speed);
+void send_serial(char c);
+char get_serial(void);
+void init_printf(void);
+int serial_string(char * str);
